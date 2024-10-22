b2b123c97c283ff6ce51141980817a31bb14c7e3
MaiconFilipiaki <31906953+MaiconFilipiaki@users.noreply.github.com>
2024-10-22 23:23:10 +0000

    add router save file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, HttpCode, HttpStatus, Post, Req, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { randomUUID } from 'crypto';
+import { extname } from 'path';
 
 @Controller()
 export class AppController {
@@ -9,4 +13,47 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post('video')
+  @HttpCode(HttpStatus.CREATED)
+  @UseInterceptors( // basicamente um middleware, mas ele validaria antes de realmente entrar em alguma camada
+    FileFieldsInterceptor( // vai pegar os campos e jogar dentro dentro da pasta /uploads
+      [
+        { name: 'video', maxCount: 1 },
+        { name: 'thumbnail', maxCount: 1 },
+      ],
+      {
+        dest: './uploads',
+        storage: diskStorage({
+          destination: './uploads',
+          filename: (_req, file, cb) => {
+            return cb(
+              null,
+              `${Date.now()}-${randomUUID()}${extname(file.originalname)}`,
+            );
+          },
+        }),
+        fileFilter: (_req, file, cb) => { // fnc pra validar tipo de entrada
+          if (file.mimetype !== 'video/mp4' && file.mimetype !== 'image/jpeg') {
+            return cb(
+              new BadRequestException(
+                'Invalid file type. Only video/mp4 and image/jpeg are supported.',
+              ),
+              false,
+            );
+          }
+          return cb(null, true);
+        },
+      },
+    ),
+  )
+  async uploadVideo(
+    @Req() _req: Request,
+    @UploadedFiles()
+    files: { video?: Express.Multer.File[]; thumbnail?: Express.Multer.File[] },
+  ): Promise<string> {
+    console.log(files);
+    return 'video uploaded';
+  }
+
 }
